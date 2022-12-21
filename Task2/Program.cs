cac7e195daea18174e5ed969da5cc9f4845bcf3c
Den <den.shibenko@mail.ru>
2022-12-21 11:32:57 +0300

    Решена задача 4. Внесены правки в задачу 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,15 +3,15 @@
 
 Console.Clear();
 
-Console.Write ("Введите первое число: ");
+Console.Write("Введите первое число: ");
 int number1 = int.Parse(Console.ReadLine()!);
-Console.Write ("Введите второе число: ");
+Console.Write("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine()!);
 int max = 0;
 
-if (number1 > number2) 
+if (number1 > number2)
 {
     max = number1;
 }
 else max = number2;
-Console.WriteLine ($"Большее число равно {max}");
+Console.WriteLine($"Большее число равно {max}");
