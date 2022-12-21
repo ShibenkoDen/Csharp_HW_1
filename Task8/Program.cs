ada02771dad01b935e859a24741906e2d7f62ddd
Den <den.shibenko@mail.ru>
2022-12-21 11:51:00 +0300

    Решена задача 8. Правки в задаче 6

---
+++ Task8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8. Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все четные числа от 1 до N
+
+// итерация + 2
+
+Console.Clear();
+
+Console.WriteLine("Введите положительное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int numberPrint = 2;
+while (numberPrint <= number)
+{
+    Console.Write(numberPrint + " ");
+    numberPrint = numberPrint + 2;
+}
+Console.WriteLine();
